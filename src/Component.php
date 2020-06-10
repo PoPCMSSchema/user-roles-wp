970cc593ba655e4cf4d1d92819cc3d094409e931
Leonardo Losoviz <leo@getpop.org>
2020-06-10 18:11:21 +0800

    Pass all skipped Component classes when initializing

--- src/Component.php
+++ src/Component.php
@@ -40,9 +40,12 @@ class Component extends AbstractComponent
     /**
      * Initialize services
      */
-    protected static function doInitialize(array $configuration = [], bool $skipSchema = false): void
-    {
-        parent::doInitialize($configuration, $skipSchema);
+    protected static function doInitialize(
+        array $configuration = [],
+        bool $skipSchema = false,
+        array $skipSchemaComponentClasses = []
+    ): void {
+        parent::doInitialize($configuration, $skipSchema, $skipSchemaComponentClasses);
         self::initYAMLServices(dirname(__DIR__));
         self::maybeInitYAMLSchemaServices(dirname(__DIR__), $skipSchema);
         ServiceConfiguration::initialize();
