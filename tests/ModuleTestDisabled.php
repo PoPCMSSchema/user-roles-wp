fdcafdc91526217613e6966116c28b7f9f4febec
leoloso <leo@getpop.org>
2023-06-08 08:30:40 +0000

    fixed cs (Renamed to disable the test)

--- tests/ModuleTestDisabled.php
+++ tests/ModuleTestDisabled.php
@@ -7,8 +7,8 @@ namespace PoPCMSSchema\UserRolesWP;
 use PoP\ComponentModel\AbstractModuleTestCase;
 
 /**
- * Made abstract (and renamed) to disable the test
+ * Renamed to disable the test
  */
-abstract class ModuleTest_ extends AbstractModuleTestCase
+class ModuleTestDisabled extends AbstractModuleTestCase
 {
 }
